99c7e4c8fe0842a244abafe2397514aaa147c171
Simone Porreca <porrecasimone@gmail.com>
2026-06-13 21:48:10 +0200

    Add API Providers

--- frontend/content/codex/infrastructure/_meta.js
+++ frontend/content/codex/infrastructure/_meta.js
@@ -1,6 +1,7 @@
 export default {
-  index: 'Overview',
-  brokers: 'Brokers',
-  taxation: 'Taxation',
-  'costs-fees': 'Costs & Fees',
-}
+  index: "Overview",
+  brokers: "Brokers",
+  taxation: "Taxation",
+  "costs-fees": "Costs & Fees",
+  "api-providers": "API Providers",
+};
